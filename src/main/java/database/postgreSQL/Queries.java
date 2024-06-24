83b7dafa08259bc867547d9b8fe1eb3eabec490f
Ryan Cummings <ryan.cummings1@montana.edu>
2024-06-24 16:47:24 -0600

    attempts to insert into pg database

--- src/main/java/database/postgreSQL/Queries.java
+++ src/main/java/database/postgreSQL/Queries.java
@@ -2,5 +2,8 @@ package database.postgreSQL;
 
 public class Queries {
     
-    private String cveTableInit = "";
+    private String cveTableInit = "CREATE TABLE if NOT EXISTS nvd_cve ( " +
+    "id SERIAL PRIMARY KEY, " +
+    "cve jsonb " +
+    ")";
 }
