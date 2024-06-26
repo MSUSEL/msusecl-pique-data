c7c07277717e4eac9bd3333ef521def60d12ac78
Ryan Cummings <ryan.cummings1@montana.edu>
2024-06-26 16:01:16 -0600

    include methods for hydrating and updating nvdMirror

--- src/main/java/presentation/PiqueNvdMirror.java
+++ src/main/java/presentation/PiqueNvdMirror.java
@@ -1,12 +1,27 @@
 package presentation;
 
+import api.cveData.NvdMirrorMetaData;
 import common.Utils;
 import service.NvdApiService;
+import service.NvdMirrorService;
+
+import java.time.Instant;
 
 public class PiqueNvdMirror {
     private static final NvdApiService nvdApiService =  new NvdApiService();
+    private static final NvdMirrorService nvdMirrorService = new NvdMirrorService();
 
     public static void buildNvdMirror(String dbContext) {
         nvdApiService.handleGetPaginatedCves(dbContext, Utils.DEFAULT_START_INDEX, Utils.NVD_MAX_PAGE_SIZE);
+    }
+
+    public static void updateNvdMirror(String dbContext) {
+        NvdMirrorMetaData metadata = nvdMirrorService.handleGetCurrentMetaData(dbContext);
+        Instant instant = Instant.now();
+        nvdApiService.handleUpdateNvdMirror(dbContext, metadata.getTimestamp(), instant.toString());
+    }
+
+    public static NvdMirrorMetaData getCurrentMetaData(String dbContext) {
+        return nvdMirrorService.handleGetCurrentMetaData(dbContext);
     }
 }
---
+++ src/main/java/service/DbContextResolver.java
@@ -0,0 +1,31 @@
+package service;
+
+import api.cveData.Cve;
+import api.cveData.NvdMirrorMetaData;
+import common.Utils;
+import database.IBulkDao;
+import database.IDao;
+import database.IMetaDataDao;
+import database.mongo.MongoBulkCveDao;
+import database.mongo.MongoCveDao;
+import database.mongo.MongoMetaDataDao;
+import database.postgreSQL.PostgresBulkCveDao;
+import database.postgreSQL.PostgresCveDao;
+import database.postgreSQL.PostgresMetaDataDao;
+
+import java.util.List;
+
+public class DbContextResolver {
+
+    public IBulkDao<List<Cve>> getBulkDao(String dbContext) {
+        return dbContext.equals(Utils.DB_CONTEXT_LOCAL) ? new MongoBulkCveDao() : new PostgresBulkCveDao();
+    }
+
+    public IMetaDataDao<NvdMirrorMetaData> getMetaDataDao(String dbContext) {
+        return dbContext.equals(Utils.DB_CONTEXT_LOCAL) ? new MongoMetaDataDao() : new PostgresMetaDataDao();
+    }
+
+    public IDao<Cve> getCveDao(String dbContext) {
+        return dbContext.equals(Utils.DB_CONTEXT_LOCAL) ? new MongoCveDao() : new PostgresCveDao();
+    }
+}
