0e21729bc7f04546705cf720aaf025c266a1cf76
Ryan <rcummings98@gmail.com>
2024-07-07 20:31:17 -0600

    A little more cleanup

--- src/main/java/presentation/NvdMirror.java
+++ src/main/java/presentation/NvdMirror.java
@@ -11,23 +11,23 @@ import java.time.Instant;
 
 public class NvdMirror {
     private static final NvdApiService nvdApiService =  new NvdApiService();
-    private static final MirrorService MIRROR_SERVICE = new MirrorService();
+    private static final MirrorService mirrorService = new MirrorService();
 
     public static void buildNvdMirror(String dbContext) {
         nvdApiService.handleGetPaginatedCves(dbContext, Constants.DEFAULT_START_INDEX, Constants.NVD_MAX_PAGE_SIZE);
     }
 
     public static void updateNvdMirror(String dbContext) throws DataAccessException {
-        NvdMirrorMetaData metadata = MIRROR_SERVICE.handleGetCurrentMetaData(dbContext);
+        NvdMirrorMetaData metadata = mirrorService.handleGetCurrentMetaData(dbContext);
         Instant instant = Instant.now();
         nvdApiService.handleUpdateNvdMirror(dbContext, metadata.getTimestamp(), instant.toString());
     }
 
     public static NvdMirrorMetaData getMetaData(String dbContext) throws DataAccessException {
-        return MIRROR_SERVICE.handleGetCurrentMetaData(dbContext);
+        return mirrorService.handleGetCurrentMetaData(dbContext);
     }
 
     public static void insertSingleCve(String dbContext, Cve cve) throws DataAccessException {
-        MIRROR_SERVICE.handleInsertSingleCve(dbContext, cve);
+        mirrorService.handleInsertSingleCve(dbContext, cve);
     }
 }
