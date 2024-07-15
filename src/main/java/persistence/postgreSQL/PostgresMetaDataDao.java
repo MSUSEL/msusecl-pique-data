8523ed11a2dea033f4fea6c5e7f5718c1dd68ef1
Ryan Cummings <ryan.cummings1@montana.edu>
2024-07-15 12:58:31 -0600

    more structure improvements

--- src/main/java/persistence/postgreSQL/PostgresMetaDataDao.java
+++ src/main/java/persistence/postgreSQL/PostgresMetaDataDao.java
@@ -1,17 +1,30 @@
 package persistence.postgreSQL;
 
+import handlers.IJsonMarshaller;
+import persistence.IDao;
+import persistence.IDataSource;
 import persistence.IMetaDataDao;
 import businessObjects.cve.NvdMirrorMetaData;
 
+import java.sql.Connection;
+
 public final class PostgresMetaDataDao implements IMetaDataDao<NvdMirrorMetaData> {
+    private final Connection conn;
+    private final IJsonMarshaller<NvdMirrorMetaData> marshaller;
+
+    public PostgresMetaDataDao(IDataSource<Connection> dataSource, IJsonMarshaller<NvdMirrorMetaData> marshaller) {
+        this.conn = dataSource.getConnection();
+        this.marshaller = marshaller;
+    }
 
     @Override
     public void updateMetaData(NvdMirrorMetaData metaData) {
-
+        //TODO implement this method
     }
 
     @Override
     public NvdMirrorMetaData fetchMetaData() {
+        //TODO implement this method
         return null;
     }
 }
